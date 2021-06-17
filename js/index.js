d7d450e3237ba94930b915e4f86e805a272d31dd
Mahim Pritom <mahimnyc@yahoo.com>
2021-06-17 14:42:56 -0400

    Iteration#1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 700;
+
+const background = new Image();
+background.src = "./images/road.png";
+
+background.onload = function () {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+};
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.querySelector("start-button").onclick = function (event) {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  }
 };
